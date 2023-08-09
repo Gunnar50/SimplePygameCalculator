9ea546706b08d6d285a11f79af5ec08540be12ed
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-09 15:13:15 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import pygame
+from settings import *
+from sprites import *
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(title)
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.screen.fill(BGCOLOUR)
+
+        pygame.display.flip()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit(0)
+
+
+game = Game()
+game.run()
